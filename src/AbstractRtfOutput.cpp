7b41dc4dbe30a9b1c05f9265316d2f03be80afbf
Sergey Katunin <sulmpx60@yandex.ru>
2026-06-26 14:26:08 +0300

    AbstractRtfOutput: split to public and private classes

--- src/AbstractRtfOutput.cpp
+++ src/AbstractRtfOutput.cpp
@@ -2,249 +2,253 @@
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
 #include "AbstractRtfOutput.h"
+#include "private/AbstractRtfOutput_p.h"
 
 /**
    Namespace for the public interface for the QRtfReader classes
 */
 namespace QRtfReader
 {
-AbstractRtfOutput::AbstractRtfOutput() = default;
+AbstractRtfOutput::AbstractRtfOutput()
+    : d(new AbstractRtfOutputPrivate)
+{
+}
 
 AbstractRtfOutput::~AbstractRtfOutput() = default;
 
 QString AbstractRtfOutput::author() const
 {
-    return m_author;
+    return d->m_author;
 }
 
 void AbstractRtfOutput::setAuthor(const QString &author)
 {
-    m_author = author;
+    d->m_author = author;
 }
 
 QString AbstractRtfOutput::companyName() const
 {
-    return m_companyName;
+    return d->m_companyName;
 }
 
 void AbstractRtfOutput::setCompanyName(const QString &companyName)
 {
-    m_companyName = companyName;
+    d->m_companyName = companyName;
 }
 
 QString AbstractRtfOutput::operatorName() const
 {
-    return m_operatorName;
+    return d->m_operatorName;
 }
 
 void AbstractRtfOutput::setOperatorName(const QString &operatorName)
 {
-    m_operatorName = operatorName;
+    d->m_operatorName = operatorName;
 }
 
 QString AbstractRtfOutput::title() const
 {
-    return m_title;
+    return d->m_title;
 }
 
 void AbstractRtfOutput::setTitle(const QString &title)
 {
-    m_title = title;
+    d->m_title = title;
 }
 
 QString AbstractRtfOutput::subject() const
 {
-    return m_subject;
+    return d->m_subject;
 }
 
 void AbstractRtfOutput::setSubject(const QString &subject)
 {
-    m_subject = subject;
+    d->m_subject = subject;
 }
 
 QString AbstractRtfOutput::comment() const
 {
-    return m_comment;
+    return d->m_comment;
 }
 
 void AbstractRtfOutput::setComment(const QString &comment)
 {
-    m_comment = comment;
+    d->m_comment = comment;
 }
 
 QString AbstractRtfOutput::documentComment() const
 {
-    return m_documentComment;
+    return d->m_documentComment;
 }
 
 void AbstractRtfOutput::setDocumentComment(const QString &documentComment)
 {
-    m_documentComment = documentComment;
+    d->m_documentComment = documentComment;
 }
 
 QString AbstractRtfOutput::managerName() const
 {
-    return m_managerName;
+    return d->m_managerName;
 }
 
 void AbstractRtfOutput::setManagerName(const QString &managerName)
 {
-    m_managerName = managerName;
+    d->m_managerName = managerName;
 }
 
 QString AbstractRtfOutput::category() const
 {
-    return m_category;
+    return d->m_category;
 }
 
 void AbstractRtfOutput::setCategory(const QString &category)
 {
-    m_category = category;
+    d->m_category = category;
 }
 
 QString AbstractRtfOutput::keywords() const
 {
-    return m_keywords;
+    return d->m_keywords;
 }
 
 void AbstractRtfOutput::setKeywords(const QString &keywords)
 {
-    m_keywords = keywords;
+    d->m_keywords = keywords;
 }
 
 QString AbstractRtfOutput::hLinkBase() const
 {
-    return m_hlinkbase;
+    return d->m_hlinkbase;
 }
 
 void AbstractRtfOutput::setHLinkBase(const QString &hLinkBase)
 {
-    m_hlinkbase = hLinkBase;
+    d->m_hlinkbase = hLinkBase;
 }
 
 QString AbstractRtfOutput::generatorInformation() const
 {
-    return m_generatorInformation;
+    return d->m_generatorInformation;
 }
 
 void AbstractRtfOutput::setGeneratorInformation(const QString &generatorInformation)
 {
-    m_generatorInformation = generatorInformation;
+    d->m_generatorInformation = generatorInformation;
 }
 
 QDateTime AbstractRtfOutput::created() const
 {
-    return m_created;
+    return d->m_created;
 }
 
 void AbstractRtfOutput::setCreatedDateTime(const QDateTime &dateTime)
 {
-    m_created = dateTime;
+    d->m_created = dateTime;
 }
 
 QDateTime AbstractRtfOutput::revised() const
 {
-    return m_revised;
+    return d->m_revised;
 }
 
 void AbstractRtfOutput::setRevisedDateTime(const QDateTime &dateTime)
 {
-    m_revised = dateTime;
+    d->m_revised = dateTime;
 }
 
 QDateTime AbstractRtfOutput::printed() const
 {
-    return m_printed;
+    return d->m_printed;
 }
 
 void AbstractRtfOutput::setPrintedDateTime(const QDateTime &dateTime)
 {
-    m_printed = dateTime;
+    d->m_printed = dateTime;
 }
 
 int AbstractRtfOutput::totalEditingTime() const
 {
-    return m_totalEditingTime;
+    return d->m_totalEditingTime;
 }
 
 void AbstractRtfOutput::setTotalEditingTime(const int totalEditingTime)
 {
-    m_totalEditingTime = totalEditingTime;
+    d->m_totalEditingTime = totalEditingTime;
 }
 
 int AbstractRtfOutput::numberOfPages() const
 {
-    return m_numberOfPages;
+    return d->m_numberOfPages;
 }
 
 void AbstractRtfOutput::setNumberOfPages(const int numberOfPages)
 {
-    m_numberOfPages = numberOfPages;
+    d->m_numberOfPages = numberOfPages;
 }
 
 int AbstractRtfOutput::numberOfWords() const
 {
-    return m_numberOfWords;
+    return d->m_numberOfWords;
 }
 
 void AbstractRtfOutput::setNumberOfWords(const int numberOfWords)
 {
-    m_numberOfWords = numberOfWords;
+    d->m_numberOfWords = numberOfWords;
 }
 
 int AbstractRtfOutput::numberOfCharacters() const
 {
-    return m_numberOfCharacters;
+    return d->m_numberOfCharacters;
 }
 
 void AbstractRtfOutput::setNumberOfCharacters(const int numberOfCharacters)
 {
-    m_numberOfCharacters = numberOfCharacters;
+    d->m_numberOfCharacters = numberOfCharacters;
 }
 
 int AbstractRtfOutput::numberOfCharactersWithoutSpaces() const
 {
-    return m_numberOfCharactersWithoutSpaces;
+    return d->m_numberOfCharactersWithoutSpaces;
 }
 
 void AbstractRtfOutput::setNumberOfCharactersWithoutSpaces(const int numberOfCharactersWithoutSpaces)
 {
-    m_numberOfCharactersWithoutSpaces = numberOfCharactersWithoutSpaces;
+    d->m_numberOfCharactersWithoutSpaces = numberOfCharactersWithoutSpaces;
 }
 
 int AbstractRtfOutput::versionNumber() const
 {
-    return m_versionNumber;
+    return d->m_versionNumber;
 }
 
 void AbstractRtfOutput::setVersionNumber(const int versionNumber)
 {
-    m_versionNumber = versionNumber;
+    d->m_versionNumber = versionNumber;
 }
 
 int AbstractRtfOutput::internalVersionNumber() const
 {
-    return m_internalVersionNumber;
+    return d->m_internalVersionNumber;
 }
 
 void AbstractRtfOutput::setInternalVersionNumber(const int internalVersionNumber)
 {
-    m_internalVersionNumber = internalVersionNumber;
+    d->m_internalVersionNumber = internalVersionNumber;
 }
 
 QVariant AbstractRtfOutput::userProp(const QString &propertyName) const
 {
-    return m_userProps.value(propertyName);
+    return d->m_userProps.value(propertyName);
 }
 
 QList<QString> AbstractRtfOutput::userPropNames() const
 {
-    return m_userProps.keys();
+    return d->m_userProps.keys();
 }
 
 void AbstractRtfOutput::addUserProp(const QString &propertyName, const QVariant &propertyValue)
 {
-    m_userProps.insert(propertyName, propertyValue);
+    d->m_userProps.insert(propertyName, propertyValue);
 }
 
 void AbstractRtfOutput::startGroup()
--- src/AbstractRtfOutput.h
+++ src/AbstractRtfOutput.h
@@ -1,8 +1,8 @@
 // SPDX-FileCopyrightText: 2008, 2010 Brad Hards <bradh@frogmouth.net>
 // SPDX-License-Identifier: LGPL-2.1-or-later
 
-#ifndef RTFREADER_RTFOUTPUTSTRATEGY_H
-#define RTFREADER_RTFOUTPUTSTRATEGY_H
+#ifndef RTFREADER_ABSTRACTRTFOUTPUT_H
+#define RTFREADER_ABSTRACTRTFOUTPUT_H
 
 #include <QColor>
 #include <QDateTime>
@@ -18,6 +18,7 @@
 */
 namespace QRtfReader
 {
+class AbstractRtfOutputPrivate;
 /**
 Abstract output strategy for RTF Parser
 */
@@ -292,74 +293,7 @@ public:
     virtual void endGroup();
 
 private:
-    // The author of the document (from document meta-data, if any)
-    QString m_author;
-
-    // The company of the author of the document (from document meta-data, if any)
-    QString m_companyName;
-
-    // The operator of the document (from document meta-data, if any)
-    QString m_operatorName;
-
-    // The title of the document (from document meta-data, if any)
-    QString m_title;
-
-    // The subject of the document (from document meta-data, if any)
-    QString m_subject;
-
-    // The category (or categories) of the document (from document meta-data, if any)
-    QString m_category;
-
-    // The manager(s) of the author of the document (from document meta-data, if any)
-    QString m_managerName;
-
-    // The comment associated with the document (from document meta-data, if any)
-    QString m_comment;
-
-    // The document comment (from document meta-data, if any)
-    QString m_documentComment;
-
-    // The keywords associated with the document (from document meta-data, if any)
-    QString m_keywords;
-
-    // The hyperlink base associated with this document (path or URL)
-    QString m_hlinkbase;
-
-    // The date and time this document was created (from document meta-data, if any)
-    QDateTime m_created;
-
-    // The date and time this document was revised (from document meta-data, if any)
-    QDateTime m_revised;
-
-    // The generator of this document (from document meta-data, if any)
-    QString m_generatorInformation;
-
-    // The date and time this document was printed (from document meta-data, if any)
-    QDateTime m_printed;
-
-    // The total editing time for the document (from document meta-data, if any)
-    int m_totalEditingTime = -1;
-
-    // The number of pages in the document (from document meta-data, if any)
-    int m_numberOfPages = -1;
-
-    // The number of words in the document (from document meta-data, if any)
-    int m_numberOfWords = -1;
-
-    // The number of characters in the document (from document meta-data, if any)
-    int m_numberOfCharacters = -1;
-
-    // The number of characters (without spaces) in the document (from document meta-data, if any)
-    int m_numberOfCharactersWithoutSpaces = -1;
-
-    // The version of the document (from document meta-data, if any)
-    int m_versionNumber = -1;
-
-    // The internal version of the document (from document meta-data, if any)
-    int m_internalVersionNumber = -1;
-
-    // User properties dictionary (from document meta-data, if any)
-    QHash<QString, QVariant> m_userProps;
+    const QScopedPointer<AbstractRtfOutputPrivate> d;
 };
 }
 
